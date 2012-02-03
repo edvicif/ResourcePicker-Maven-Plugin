88888c0751481a82a80fd34ac69175d26a129a40
edvicif <edvicif@gmail.com>
2012-02-03 00:10:02 +0000

    tester project

---
+++ PluginTester/src/main/java/com/mycompany/R.java
@@ -0,0 +1,9 @@
+package com.mycompany;
+public enum R {
+
+SAMPLE_XML("sample.xml"),SAMPLE_1_XML("sample_1.xml"),;
+
+private R(String fileName) {
+}
+
+}
---
+++ PluginTester/src/main/java/com/mycompany/hellotest/App.java
@@ -0,0 +1,18 @@
+package com.mycompany.hellotest;
+
+import com.mycompany.R;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        
+    }
+}
