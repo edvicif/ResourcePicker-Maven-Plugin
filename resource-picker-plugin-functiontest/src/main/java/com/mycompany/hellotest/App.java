4225ca2f0701c2bb50b3f1eee3bb6cab99dc2cf3
David Cifer <kozod@sincs.hozza>
2012-03-26 23:43:19 +0100

    #15 Plugintester project turned into function test, which try to reach resources

--- resource-picker-plugin-functiontest/src/main/java/com/mycompany/hellotest/App.java
+++ resource-picker-plugin-functiontest/src/main/java/com/mycompany/hellotest/App.java
@@ -10,7 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        //This assure successfull compilation
+        sub.R r = sub.R.SAMPLE_XML;
         
     }
 }
